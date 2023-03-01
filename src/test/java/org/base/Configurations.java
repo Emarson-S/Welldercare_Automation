ddd21c7d7855b803a391e4a351430a4372f12693
Emarson-S <emarson.s@mavens-i.com>
2023-03-01 19:51:52 +0530

    add some methods

--- src/test/java/org/base/Configurations.java
+++ src/test/java/org/base/Configurations.java
@@ -1,5 +1,6 @@
 package org.base;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
@@ -30,7 +31,7 @@ public class Configurations extends BaseClass {
 		driver.get(props.getProperty(url));
 	}
 
-//  <--- Launch token response URL from Application properties --->		
+//  <--- Take response from verification token url --->		
 
 	public static void readtoken(String ActiveToken, String email, String SetPassword) throws IOException {
 		FileReader reader = new FileReader(
@@ -42,7 +43,7 @@ public class Configurations extends BaseClass {
 		driver.get(props.getProperty(SetPassword) + token + "");
 	}
 
-//  <--- Launch OTP response URL from Application properties --->
+//  <--- Take otp from otp url --->		
 
 	public static String readOtp(String Otp, String email) throws IOException {
 		FileReader reader = new FileReader(
@@ -52,6 +53,45 @@ public class Configurations extends BaseClass {
 		Response respon = RestAssured.get(Otp + email + "");
 		String s = respon.asString();
 		return s;
+	}
+	
+	
+//	<--- Take token from verification token table --->		
+	
+	public static void readVerificationToken(String email, String SetPassword) throws IOException {
+		FileReader reader = new FileReader(
+				"C:\\Users\\Emarson\\eclipse-workspace\\Welldercare\\src\\test\\java\\org\\runner\\"+host);
+		Properties props = new Properties();
+		props.load(reader);
+		String token=MongoDBCollections.connectMongoDB("DB_URL", "Database", MongoDBCollections.TBL_VERIFICATION_TOKEN, "user.emailId", email, "token");
+		System.out.println(token);
+		driver.get(props.getProperty(SetPassword) + token + "");
+	}
+	
+	
+//	<--- Take token from reset password token table --->	
+	
+	public static void readPasswordToken(String email, String ResetPassword) throws IOException {
+		FileReader reader = new FileReader(
+				"C:\\Users\\Emarson\\eclipse-workspace\\Welldercare\\src\\test\\java\\org\\runner\\"+host);
+		Properties props = new Properties();
+		props.load(reader);
+		String token=MongoDBCollections.connectMongoDB("DB_URL", "Database", MongoDBCollections.TBL_PASSWORD_RESET_TOKEN, "user.emailId", email, "token");
+		System.out.println(token);
+		driver.get(props.getProperty(ResetPassword) + token + "");
+	}
+	
+	
+//	<--- Take otp from otp table --->
+	
+	public static String readOtpFromTable(String email) throws IOException {
+		FileReader reader = new FileReader(
+				"C:\\Users\\Emarson\\eclipse-workspace\\Welldercare\\src\\test\\java\\org\\runner\\"+host);
+		Properties props = new Properties();
+		props.load(reader);
+		String otp=MongoDBCollections.connectMongoDB("DB_URL", "Database", MongoDBCollections.TBL_ONE_TIME_PASSWORD, "emailId", email, "otp");
+		System.out.println(otp);
+		return otp;
 	}
 
 }
